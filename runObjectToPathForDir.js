9c5bec8f35ceda9904aa02f92d780dee1d444c5d
Hunter Hodnett <hchodnett@gmail.com>
2020-01-02 15:12:14 -0600

    Preserve files in generatedDXF folder

--- runObjectToPathForDir.js
+++ runObjectToPathForDir.js
@@ -9,75 +9,86 @@ const RUN_OBJECT_TO_PATH_FOR_FILE_LOCATION = 'runObjectToPathForFile.bat';
 let generatedDXFDirectory;
 const args = process.argv.slice(2);
 if (!args[0]) {
-    console.log(`no path provided for the directory containing the generated DXF files, defaulting to ${ DEFAULT_GENERATED_DXF_DIR }`);
-    generatedDXFDirectory = DEFAULT_GENERATED_DXF_DIR;
+  console.log(
+    `no path provided for the directory containing the generated DXF files, defaulting to ${DEFAULT_GENERATED_DXF_DIR}`
+  );
+  generatedDXFDirectory = DEFAULT_GENERATED_DXF_DIR;
 } else {
-    generatedDXFDirectory = args[0];
+  generatedDXFDirectory = args[0];
 }
 
 function getDateTime() {
-    const date = new Date();
-    let hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-    let min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-    let sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-    const year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-    let day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
+  const date = new Date();
+  let hour = date.getHours();
+  hour = (hour < 10 ? '0' : '') + hour;
+  let min = date.getMinutes();
+  min = (min < 10 ? '0' : '') + min;
+  let sec = date.getSeconds();
+  sec = (sec < 10 ? '0' : '') + sec;
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  month = (month < 10 ? '0' : '') + month;
+  let day = date.getDate();
+  day = (day < 10 ? '0' : '') + day;
 
-    return year + "-" + month + "-" + day + "-" + hour + "-" + min + "-" + sec;
+  return year + '-' + month + '-' + day + '-' + hour + '-' + min + '-' + sec;
 }
-const processedDXFFilesDirName = `${ PROCESSED_DXF_FILES_DIR_NAME_PREFIX }_${ getDateTime() }/`;
+const processedDXFFilesDirName = `${PROCESSED_DXF_FILES_DIR_NAME_PREFIX}_${getDateTime()}/`;
 
-const spawnProcessAndAwaitCompletion = async (processToBeSpawned, processArguments) =>
-    new Promise((resolve, reject) => {
-        const ls = spawn(processToBeSpawned, processArguments);
+const spawnProcessAndAwaitCompletion = async (
+  processToBeSpawned,
+  processArguments
+) =>
+  new Promise((resolve, reject) => {
+    const ls = spawn(processToBeSpawned, processArguments);
 
-        ls.stdout.on('data', function (data) {
-            console.log('stdout: ' + data);
-        });
-
-        ls.stderr.on('data', function (data) {
-            console.log('stderr: ' + data);
-            throw data;
-        });
-
-        ls.on('exit', function (code) {
-            console.log('child process exited with code ' + code);
-            resolve();
-        });
+    ls.stdout.on('data', function(data) {
+      console.log('stdout: ' + data);
     });
 
+    ls.stderr.on('data', function(data) {
+      console.log('stderr: ' + data);
+      throw data;
+    });
+
+    ls.on('exit', function(code) {
+      console.log('child process exited with code ' + code);
+      resolve();
+    });
+  });
+
 async function stepThroughFiles(files) {
-    const fileName = files.shift();
-    const fullFilePath = generatedDXFDirectory + fileName;
-    await spawnProcessAndAwaitCompletion(RUN_OBJECT_TO_PATH_FOR_FILE_LOCATION, [fullFilePath]);
-    fs.renameSync(fullFilePath, processedDXFFilesDirName + fileName);
-    
-    if (files.length) {
-        stepThroughFiles(files);
-    } else {
-        console.log('processing complete');
-        console.log(`you can view the processed files by opening the folder located at ${ __dirname }\\${ processedDXFFilesDirName }`);
-    }
+  const fileName = files.shift();
+  const fullFilePath = generatedDXFDirectory + fileName;
+  await spawnProcessAndAwaitCompletion(RUN_OBJECT_TO_PATH_FOR_FILE_LOCATION, [
+    fullFilePath,
+  ]);
+  fs.copyFileSync(fullFilePath, processedDXFFilesDirName + fileName);
+
+  if (files.length) {
+    stepThroughFiles(files);
+  } else {
+    console.log('processing complete');
+    console.log(
+      `you can view the processed files by opening the folder located at ${__dirname}\\${processedDXFFilesDirName}`
+    );
+  }
 }
 
 fs.readdir(generatedDXFDirectory, async (err, files) => {
-    if (files.length === 0) {
-        console.error(`ERROR: no files found in directory: ${ generatedDXFDirectory }`);
-        process.exit(1);
-    }
+  if (files.length === 0) {
+    console.error(
+      `ERROR: no files found in directory: ${generatedDXFDirectory}`
+    );
+    process.exit(1);
+  }
 
-    fs.mkdirSync(processedDXFFilesDirName);
-    try {
-        await stepThroughFiles(files);
-    } catch (err) {
-        // Delete processed directory since an error occurred and results are likely corrupted
-        rimraf.sync(processedDXFFilesDirName);
-        process.exit(1);
-    }
+  fs.mkdirSync(processedDXFFilesDirName);
+  try {
+    await stepThroughFiles(files);
+  } catch (err) {
+    // Delete processed directory since an error occurred and results are likely corrupted
+    rimraf.sync(processedDXFFilesDirName);
+    process.exit(1);
+  }
 });
